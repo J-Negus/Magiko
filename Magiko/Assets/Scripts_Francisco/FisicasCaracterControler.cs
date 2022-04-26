66f87d54be16b60e439d78afea8ac3d2eb925425
Rokdox <103514031+Rokdox@users.noreply.github.com>
2022-04-26 20:51:33 +0200

    Cubos
    Scripts de cubo amarillo y rojo

--- Magiko/Assets/Scripts_Francisco/FisicasCaracterControler.cs
+++ Magiko/Assets/Scripts_Francisco/FisicasCaracterControler.cs
@@ -14,6 +14,18 @@ public class FisicasCaracterControler : MonoBehaviour
 
     private CharacterController _characterControler;
 
+    //Cambio de color de caja
+    Color cambioRojo = Color.red;
+    Color cambioAmarillo = Color.yellow;
+
+
+    public Renderer _renderer;
+
+    public GameObject cuboRojo;
+    public GameObject cuboAmarillo;
+
+
+   
     void Start()
     {
         _characterControler = GetComponent<CharacterController>();
@@ -22,6 +34,8 @@ public class FisicasCaracterControler : MonoBehaviour
         {
             Debug.Log("Character controler es Nulo");
         }
+
+        _renderer = GetComponent<Renderer>();
     }
 
     void Update()
@@ -45,12 +59,31 @@ public class FisicasCaracterControler : MonoBehaviour
         _characterControler.Move(velocity * Time.deltaTime);
         _characterControler.Move(velocity * Time.deltaTime);
     }
+
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (hit.collider.tag.Equals("Enemy"))
         {
         Destroy(hit.collider.gameObject);
         }
+
+
+        if (hit.collider.tag.Equals("Enemy_Color"))
+        {
+            cuboRojo.GetComponent<Renderer>().material.color = cambioRojo;
+        }
+
+
+        if (hit.collider.tag.Equals("Enemy_ColorTime"))
+        {          
+            Invoke("CambioAmarillo",2f);           
+        }
+
     }
+
+     void CambioAmarillo()
+     {
+        cuboAmarillo.GetComponent<Renderer>().material.color = cambioAmarillo;  
+     }
 
 }
